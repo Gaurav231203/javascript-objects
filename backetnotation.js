0cf6aa9afdfac2e494f53d9b0f258a9e63556020
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-02 00:59:27 +0530

    Added new files

---
+++ backetnotation.js
@@ -0,0 +1,6 @@
+let obj = {
+    name:"gauarv",
+    age :22,
+}
+let obj2="name";
+console.log(obj['name']);
--- keyvalue.js
+++ keyvalue.js
@@ -2,4 +2,4 @@ let obj={
     name: "gaurav"
     //keuy is left side of collen : right side is value
 }
-obj.name
+ console.log(obj.name)
