f722aef0ff7a5922dd84516bb9909128b512a3ee
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-03 00:47:07 +0530

    Added new files

---
+++ deepcloneing.js
@@ -0,0 +1,21 @@
+let info ={
+    name: "gaurav",
+    age:22,
+    year:2026,
+    address:{
+
+        city:"ksp",
+    },
+};
+/*let obj2= structuredClone(info);
+ obj2.address.city ="rampur";
+ console.log(info.address.city);
+  console.log(obj2.address.city);// this is the new way in jsE6*/
+
+
+  //now older verson
+  //using JSON
+let obj2 =JSON.parse(JSON .stringify(info));
+obj2.address.city="rampur";
+
+console.log(obj2);
