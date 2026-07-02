ba48432f292f8adb4c19f8a9ce8ff7b69d2a2536
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-02 11:03:51 +0530

    Added new files

---
+++ objectdotkeys.js
@@ -0,0 +1,12 @@
+/*Object.keys()
+
+Ye object ki saari keys ko array ke form mein return karta hai.*/
+let obj={
+    name:"gamurav",
+    age:22,
+    email:"bishtgaurav222@gmail.com"
+
+};
+//this is Object.key();
+let keys = Object.keys(obj);
+console.log(keys);
