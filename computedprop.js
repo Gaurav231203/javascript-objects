8a4154bfc2cfcca860b2b2cb9540fd029232ff96
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-03 01:58:34 +0530

    Added new files

---
+++ computedprop.js
@@ -0,0 +1,17 @@
+
+
+let obj ="admin";
+
+let info ={
+    name: "gaurav",
+    age:22,
+    year:2026,
+    address:{
+
+        city:"ksp",
+    },
+    [obj] : "gaurav",
+};
+
+
+console.log(info);
