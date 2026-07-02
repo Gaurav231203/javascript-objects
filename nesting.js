826136132f06f1fa5fb1364b89d94c4418389be9
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-02 10:28:18 +0530

    Added new files

---
+++ nesting.js
@@ -0,0 +1,13 @@
+let obj={
+    name: "gaurav",
+    address: {
+      city:"kashipur",
+       pincode: 240714,
+       location:{
+          lng: 77.77,
+          lat:18.18,
+       },
+
+    },
+};
+console.log(obj.address.location.lat)
